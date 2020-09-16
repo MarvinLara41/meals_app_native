1b2c23ac36381925f2439d91287ef668855e1d4e
marvin lara <laramarvin105@gmail.com>
2020-09-16 15:13:10 -0400

    adding navigation

--- client/screens/MealDetailScreen.js
+++ client/screens/MealDetailScreen.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const MealDetailsScreen = (props) => {
-	<View style={styles.screen}>
-		<Text>MealDetailsScreen </Text>
-	</View>;
+	return (
+		<View style={styles.screen}>
+			<Text>MealDetailsScreen </Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
